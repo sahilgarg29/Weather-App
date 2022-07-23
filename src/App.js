af7c18c849729d08069fc28e57e2f14a880b7893
Sahil Garg <gargs2483@gmail.com>
2022-07-23 12:05:02 +0530

    fetch user coordinates with geolocation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar';
 
 function App() {
 
   const [location, setLocation] = useState("Bangalore");
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+        console.log('latitude', position.coords.latitude, 'longitude', position.coords.longitude);
+       },
+      (error_message) => {
+        alert('An error has occured while retrieving location', error_message)
+      });
+    } else {
+      console.log('geolocation is not enabled on this browser')
+    }
+  }, [])
+
 
   return (
     <div className='app'>
