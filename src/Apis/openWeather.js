858f00e2b60b4f045fd432fc837f928d92b00818
Sahil Garg <gargs2483@gmail.com>
2022-07-23 14:43:37 +0530

    created a function that fetch hourly and daily data with coordinates

---
+++ src/Apis/openWeather.js
@@ -0,0 +1,10 @@
+export const fetchHourlyDailyWithCoordinartes = async (lat, long) => {
+  const token = "e4c70ce6a6821649a416cb9521d5f4f8";
+  console.log(token);
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,alerts&units=metric&appid=${token}`;
+  console.log(url);
+  let res = await fetch(url);
+  res = await res.json();
+  console.log(res)
+  return res;
+}
