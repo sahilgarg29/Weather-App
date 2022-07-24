a71070491d579807c50ed26ad2fe85d884f19976
Sahil Garg <gargs2483@gmail.com>
2022-07-24 10:56:04 +0530

    show correct data to temperature card

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import SearchBar from './Components/SearchBar';
 import TemperatureCard from './Components/TempratureCard';
 import WeatherChart from './Components/WeatherChart';
 import Clouds from './images/clouds.png'
+import correctImage from './Utils/correctWeatherImage';
 
 function App() {
 
   const [location, setLocation] = useState("Bangalore");
   const [forcastData, setForcastData] = useState(null);
   const [selectedDay, setSelectedDay] = useState(0);
+
   useEffect(() => {
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -54,7 +56,7 @@ function App() {
       <SearchBar location={location} onLocationChange={setLocation} />
       <Dailyforcast dailyData={forcastData? forcastData.daily: []} onDayChange={setSelectedDay} selectedDay={selectedDay}/>
       <div className='currentForcast'>
-        <TemperatureCard temp="39" icon={Clouds}/>
+        <TemperatureCard temp={forcastData ? Math.round(forcastData.hourly[0].temp) : "24"} icon={forcastData? correctImage(forcastData.hourly[0].weather[0].icon): ""}/>
         <WeatherChart series={convertToArray(filterDataByDate(forcastData? forcastData.hourly: [], selectedDay))} labels={convertToTimeArray(filterDataByDate(forcastData? forcastData.hourly: [], selectedDay))}/>
       </div>
     </div>
